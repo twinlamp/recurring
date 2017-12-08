c6ef67193e63a258b6cd66b24856be37f0d498f7
twinlamp <vyboyshchik@gmail.com>
2017-12-08 20:02:55 +0300

    orders multiple filters per field, admin item routing

--- app/assets/javascripts/orders.js
+++ app/assets/javascripts/orders.js
@@ -1,19 +1,37 @@
 var orders_table = null;
 $(document).on('turbolinks:load', function() {
   if ($('#orders-table').length != 0) {
+    var common_filters = [];
     orders_table = $('#orders-table').dataTable({
       "initComplete": function () {
-        var self = this;
-        $('.filters input, .filters select').on('change', function(e) {
-          th = $(e.target).closest("th")
-          self.api().column(th.index()).search($(e.target).val()).draw()   
-        });
+        var self = this
+        $('#orders-table').on('filters-update', function() {
+          self.api().ajax.reload()
+        })
       },
       "processing": true,
       "serverSide": true,
       "responsive": true,
       "sServerMethod": 'POST',
-      "ajax": $('#orders-table').data('source'),
+      "ajax": {
+        "url": $('#orders-table').data('source'),
+        "data": function ( d ) {
+          d.filters = common_filters;
+        }
+      },
+      stateSave: true,
+      stateSaveCallback: function(settings,data) {
+        data.filters = common_filters;
+        key = 'DataTables_' + settings.sInstance + '_' + window.location.pathname
+        localStorage.setItem( key, JSON.stringify(data) )
+      },
+      stateLoadCallback: function(settings) {
+        key = 'DataTables_' + settings.sInstance + '_' + window.location.pathname
+        data = localStorage.getItem( key );
+        if (data.filters) { common_filters = data.filters; }
+        $('#orders-table').trigger('filters-restore')
+        return JSON.parse( data )
+      },
       "pagingType": "full_numbers",
       "autoWidth": false,
       "sDom": 'rt<"row"<"col-sm-2"l><"col-sm-5"i><"col-sm-5"p>>',
@@ -29,6 +47,185 @@ $(document).on('turbolinks:load', function() {
         {"data": "state", className: "all"},
         {"data": "dropdown", sortable: false, className: "all"}
       ]
+    });
+    $(function() {
+
+      var FORMBUILDER = FORMBUILDER || {};
+
+      FORMBUILDER.filters = common_filters;
+      FORMBUILDER.saved_filters = [];
+      FORMBUILDER.current_filter = {};
+      FORMBUILDER.possible_column_filters = {
+        number: ['Equal to', 'Contains', 'Begins with', 'Ends with', 'Not equal to'],
+        account: ['Equal to', 'Contains', 'Begins with', 'Ends with', 'Not equal to'],
+        state: ['Equal to', 'Contains', 'Begins with', 'Ends with', 'Not equal to'],
+        submitted_at: ['Equal to', 'Greater than', 'Less than'],
+        sub_total: ['Equal to', 'Greater than', 'Less than']
+      }
+
+      FORMBUILDER.actions = {
+
+        init: function(){
+
+          FORMBUILDER.actions.addFilter();
+          FORMBUILDER.actions.removeFilter();
+          FORMBUILDER.actions.validateForm();
+          FORMBUILDER.actions.fieldValid(key);
+          FORMBUILDER.actions.clearErrors();
+          FORMBUILDER.actions.renderFilters();
+          FORMBUILDER.actions.resetForm();
+          FORMBUILDER.actions.updateCurrentFilter();
+
+        },
+
+        addFilter: function(){
+          FORMBUILDER.actions.clearErrors();
+          if (FORMBUILDER.actions.validateForm()) {
+            FORMBUILDER.filters.push($.extend({}, FORMBUILDER.current_filter));
+            FORMBUILDER.actions.resetForm();
+            FORMBUILDER.actions.renderFilters();
+          }
+        },
+
+        removeFilter: function(e){
+          var index = parseInt($(e.target).closest('tr').data( "index" ));
+          FORMBUILDER.filters.splice(index, 1);
+          FORMBUILDER.actions.resetForm();
+          FORMBUILDER.actions.renderFilters();
+        },
+
+        validateForm: function(){
+          var valid = true;
+          ['column_name', 'filter', 'value'].forEach(function(key){
+            if (!FORMBUILDER.actions.fieldValid(key)) { 
+              $('#add_filter_form #' + key).parent().addClass('has-error');
+              valid = false;
+            }
+          })
+          return valid;
+        },
+
+        fieldValid: function(key) {
+          var value = FORMBUILDER.current_filter[key]
+          switch(key) {
+            case 'column_name':
+              return value
+              break;
+            case 'filter':
+              return value
+              break;
+            case 'value':
+              return value
+              break;
+          }
+        },
+
+        clearErrors: function() {
+          $('#add_filter_form td').removeClass('has-error');
+        },
+
+        renderFilters: function() {
+          $('tr.filter_data').remove();
+          FORMBUILDER.filters.forEach(function(el, i) {
+            var item = document.createElement("tr");
+            item.className = "filter_data";    
+            item.setAttribute('data-index', i)
+            $(item)
+            .append(
+              $("<td class='column_name'>")
+              .append(
+                el.column_name
+              )
+            )
+            .append(
+              $("<td class='filter'>")
+              .append(
+                el.filter
+              )
+            )
+            .append(
+              $("<td class='value'>")
+              .append(
+                el.value
+              )
+            )
+            .append(
+              $("<td span='10%' class='text-center'>")
+              .append(
+                $('<a href="#" class="remove-filter"><i class="fa fa-trash"></i></a>')
+              )
+            )
+            $('.column-filters').prepend(item)
+          })
+          $('.remove-filter').click(function(e){
+            e.stopPropagation();
+            e.preventDefault();
+            FORMBUILDER.actions.removeFilter(e);
+          })
+        },
+
+        resetForm: function() {
+          $('#add_filter_form :input').val('').trigger('change');
+          FORMBUILDER.actions.updateCurrentFilter();
+        },
+
+        updateCurrentFilter: function() {
+          var filter = FORMBUILDER.current_filter;
+          filter.column_name = $('#add_filter_form :input#column_name').val();
+          filter.filter = $('#add_filter_form :input#filter').val();
+          filter.value = $('#add_filter_form :input#value').val();
+        },
+
+        setValueFieldType: function() {
+          if (FORMBUILDER.current_filter['column_name'] == 'submitted_at') {
+            $('#add_filter_form :input#value').datepicker();
+          } else {
+            $('#add_filter_form :input#value').datepicker('destroy');
+          }
+        }
+
+      }
+
+      $('.add-filter').click(function(e){
+        e.stopPropagation();
+        e.preventDefault();
+        FORMBUILDER.actions.addFilter();
+      })
+
+      $('#add_filter_form :input').change(function(){
+        FORMBUILDER.actions.updateCurrentFilter();
+      })
+
+      $("#add_filter_form :input#column_name").on('change', function(e){
+        var column_name = $("#add_filter_form :input#column_name").val();
+        var $el = $(":input#filter");
+        $el.empty();
+        $el.append($("<option value></option>"));
+        var filters = FORMBUILDER.possible_column_filters[column_name]
+        $.each(filters, function(index,value) {
+          $el.append($("<option></option>")
+             .attr("value", value).text(value));
+        });
+        $("#add_filter_form :input#value").val('');
+        FORMBUILDER.actions.setValueFieldType();
+      });
+
+      $('button#update_filters').click(function(){
+        common_filters = JSON.parse(JSON.stringify(FORMBUILDER.filters));
+        $('#orders-table').trigger('filters-update')
+        $('#filterModal').modal("toggle")
+      })
+
+      $('button#cancel_filters').click(function(){
+        FORMBUILDER.filters = JSON.parse(JSON.stringify(common_filters));
+        FORMBUILDER.actions.renderFilters();
+        $('#filterModal').modal("toggle")
+      })
+
+      $('#orders-table').on('filters-restore', function() {
+        FORMBUILDER.filters = JSON.parse(JSON.stringify(common_filters));
+        FORMBUILDER.actions.renderFilters();
+      })
     });
   }
 });
